5cd42797be16889d595caee7e98e6eed607039db
Jacob Klymenko <klymenkojacob@gmail.com>
2025-04-16 19:11:28 -0700

    feat: add continue reading card structure

--- client/lib/widgets/continue_reading.dart
+++ client/lib/widgets/continue_reading.dart
@@ -1,10 +1,72 @@
 import 'package:flutter/material.dart';
 
-class ContinueReading extends StatelessWidget {
+class ContinueReading extends StatefulWidget {
   const ContinueReading({super.key});
 
   @override
+  State<ContinueReading> createState() => _ContinueReadingState();
+}
+
+class _ContinueReadingState extends State<ContinueReading> {
+  // late bool _loading;
+  double _progressValue = 0.6;
+
+  @override
+  void initState() {
+    super.initState();
+    // _loading = false;
+    // _progressValue = 0.6;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      elevation: 2,
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Continue Reading?",
+                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text("John - Chapter 3", style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                IconButton(
+                  icon: Icon(Icons.play_circle_outline_sharp, size: 45),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            SizedBox(height: 16),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(
+                    Theme.of(context).colorScheme.primary,
+                  ),
+                  value: _progressValue,
+                ),
+                SizedBox(height: 4),
+                Text('${(_progressValue * 100).round()}%'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- client/lib/widgets/daily_verse.dart
+++ client/lib/widgets/daily_verse.dart
@@ -5,52 +5,34 @@ class DailyVerse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Daily Verse",
-            style: Theme.of(
-              context,
-            ).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 4),
-          Card(
-            elevation: 2,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+    return Card(
+      margin: EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 8),
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Daily Verse",
+              style: Theme.of(
+                context,
+              ).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w500),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '"For God so loved the world that He gave His one and only Son..."',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    SizedBox(height: 12),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "John 3:16",
-                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+            SizedBox(height: 8),
+            Text(
+              '"For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life."',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 8),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                "John 3:16",
+                style: TextStyle(fontStyle: FontStyle.italic),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
